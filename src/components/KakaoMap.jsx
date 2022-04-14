f1933b332c7e75f139728aa2eea64ad1ab32b0fb
sewon <shin6403@gmail.com>
2022-04-14 23:16:54 +0900

    wip

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect } from "react";
-import styled from "styled-components";
 
 const { kakao } = window;
 
-function KakaoMap(props) {
+function KakaoMap() {
   useEffect(() => {
     const container = document.getElementById("myMap");
-
-    console.log("@@@");
 
     const options = {
       center: new kakao.maps.LatLng(33.450701, 126.570667),
       level: 3,
     };
+
+    const map = new kakao.maps.Map(container, options);
   }, []);
 
   return (
